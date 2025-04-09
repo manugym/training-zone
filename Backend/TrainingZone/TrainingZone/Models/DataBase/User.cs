ae87a991ca8ed6a3920e8cbe42309fc6abdc05ae
AGALMAD <agalmad@g.educaand.es>
2025-04-09 16:34:21 +0200

    base de back

---
+++ Backend/TrainingZone/TrainingZone/Models/DataBase/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TrainingZone.Models.DataBase;
+
+[Index(nameof(Email), IsUnique = true)]
+[Index(nameof(Phone), IsUnique = true)]
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+    public string? AvatarImageUrl { get; set; }
+    
+}
---
+++ Backend/TrainingZone/TrainingZone/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using TrainingZone.Models.DataBase;
+using TrainingZone.Repositories.Base;
+
+namespace TrainingZone.Repositories;
+
+public class UserRepository: Repository<User,int>
+{
+    public UserRepository(TrainingZoneContext context) : base(context) { }
+
+}
