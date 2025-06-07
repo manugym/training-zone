2e55edbea0d96b926f4642e3f542485bd1fe7e95
manumuno <manueljmarin@outlook.com>
2025-06-07 21:01:39 +0200

    schedule request

--- Frontend/training-zone/src/services/class.service.ts
+++ Frontend/training-zone/src/services/class.service.ts
@@ -1,9 +1,11 @@
 import { Class } from "../models/class";
+import { Schedule } from "../models/schedule";
 import apiService from "./api.service";
 
 class ClassService {
   private readonly ALL_CLASSES_URL = "Class/getAll"
   private readonly CLASS_URL = "Class"
+  private readonly SCHEDULE_BY_DATE = "Schedule/byDate"
 
   async getAllClasses(): Promise<Class[]> {
     const response = await apiService.get<Class[]>(this.ALL_CLASSES_URL);
@@ -24,6 +26,25 @@ class ClassService {
 
     return response.data;
   }
+
+  async getClassesByDate(classId: string, date: string): Promise<Schedule[]> {
+    const response = await apiService.get<Schedule[]>(`${this.SCHEDULE_BY_DATE}?classId=${classId}&date=${date}`);
+
+    if (!response.success) {
+      throw new Error("Failed to fetch schedules: " + response.error);
+    }
+
+    const schedules = response.data.map((s) => ({
+      ...s,
+      StartDateTime: new Date(s.StartDateTime),
+      EndDateTime: new Date(s.EndDateTime)
+    }))
+    return schedules;
+  }
+
+
+
 }
+
 
 export default new ClassService();
