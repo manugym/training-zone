6b1f717e12a3c937b469c71010e2e86406b7c4ea
manumuno <manueljmarin@outlook.com>
2025-06-11 01:59:13 +0200

    schedule service updated

--- Frontend/training-zone/src/services/schedule.service.ts
+++ Frontend/training-zone/src/services/schedule.service.ts
@@ -1,13 +1,14 @@
 import apiService from "./api.service";
 import { CreateSchedule } from "../models/create-schedule";
 import { UpdateSchedule } from "../models/update-schedule";
-import { Schedule } from "../models/schedule";
+import { ScheduleDto } from "../models/schedule-dto";
+import { TrainerDto } from "../models/trainer-dto";
 
 class ScheduleService {
   private readonly SCHEDULE_URL = "Schedule"
 
-  async createSchedule(newSchedule: CreateSchedule): Promise<Schedule> {
-    const response = await apiService.post<Schedule>(this.SCHEDULE_URL, newSchedule);
+  async createSchedule(newSchedule: CreateSchedule): Promise<ScheduleDto> {
+    const response = await apiService.post<ScheduleDto>(this.SCHEDULE_URL, newSchedule);
 
     if(!response.success) {
       throw new Error("Failed to create new schedule: " + response.error); 
@@ -16,8 +17,8 @@ class ScheduleService {
     return response.data;
   }
 
-  async deleteSchedule(id: number): Promise<Schedule> {
-    const response = await apiService.delete<Schedule>(`${this.SCHEDULE_URL}/${id}`);
+  async deleteSchedule(id: number): Promise<ScheduleDto> {
+    const response = await apiService.delete<ScheduleDto>(`${this.SCHEDULE_URL}/${id}`);
   
     if(!response.success){
       throw new Error("Failed to delete schedule: " + response.error);
@@ -26,8 +27,8 @@ class ScheduleService {
     return response.data;
   }
 
-  async updateSchedule(id: number, updateSchedule: UpdateSchedule): Promise<Schedule> {
-    const response = await apiService.put<Schedule>(`${this.SCHEDULE_URL}/${id}`, updateSchedule);
+  async updateSchedule(id: number, updateSchedule: UpdateSchedule): Promise<ScheduleDto> {
+    const response = await apiService.put<ScheduleDto>(`${this.SCHEDULE_URL}/${id}`, updateSchedule);
   
     if(!response.success){
       throw new Error("Failed to update schedule: " + response.error);
@@ -36,11 +37,21 @@ class ScheduleService {
     return response.data;
   }
 
-  async getAllSchedules(): Promise<Schedule[]> {
-    const response = await apiService.get<Schedule[]>(`${this.SCHEDULE_URL}/schedules`)
+  async getAllSchedules(): Promise<ScheduleDto[]> {
+    const response = await apiService.get<ScheduleDto[]>(`${this.SCHEDULE_URL}/schedules`)
   
     if(!response.success){
       throw new Error("Failed to fetch all schedules: " + response.error);
+    }
+
+    return response.data;
+  }
+
+  async getAllTrainers(): Promise<TrainerDto[]> {
+    const response = await apiService.get<TrainerDto[]>(`${this.SCHEDULE_URL}/trainers`)
+  
+    if(!response.success){
+      throw new Error("Failed to fetch all trainers: " + response.error);
     }
 
     return response.data;
