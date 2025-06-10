cfdd1daf12875236b3e7c8a0feba9aa3217114ad
manumuno <manueljmarin@outlook.com>
2025-06-10 15:56:40 +0200

    reservation service and interfaces

--- App/training-zone/app/(tabs)/AllClasses.tsx
+++ App/training-zone/app/(tabs)/AllClasses.tsx
@@ -55,7 +55,7 @@ export default function AllClassesScreen() {
                 key={activity.Id}
                 style={[styles.classCard, { backgroundColor: theme.details, borderColor: theme.primary }]}
                 activeOpacity={0.9}
-                onPress={() => router.push({ pathname: "/class", params: { id: activity.Id } })}
+                onPress={() => router.push({ pathname: "/ClassDetail", params: { id: activity.Id } })}
               >
                 <Image
                   source={{
@@ -73,7 +73,7 @@ export default function AllClassesScreen() {
                   </ThemedText>
                   <TouchableOpacity
                     style={[styles.button, { backgroundColor: theme.primary }]}
-                    onPress={() => router.push({ pathname: "/class", params: { id: activity.Id } })}
+                    onPress={() => router.push({ pathname: "/ClassDetail", params: { id: activity.Id } })}
                   >
                     <Text style={styles.buttonText}>Ver Clase</Text>
                   </TouchableOpacity>
