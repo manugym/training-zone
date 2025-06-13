6145d278010d215e8be8d39a4cf41f3f1eadfd4f
AGALMAD <agalmad@g.educaand.es>
2025-06-13 11:54:11 +0200

    paddigs in views

--- App/training-zone/app/(tabs)/AllConversations.tsx
+++ App/training-zone/app/(tabs)/AllConversations.tsx
@@ -20,6 +20,7 @@ import chatService from "@/services/chat.service";
 import websocketService from "@/services/websocket.service";
 import userService from "@/services/user.service";
 import { Shapes } from "@/constants/Shapes";
+import { ThemedView } from "@/components/ThemedView";
 
 export default function AllConversations() {
   const SERVER_IMAGE_URL = `${ServerUrl}/UserProfilePicture`;
@@ -61,131 +62,138 @@ export default function AllConversations() {
   return (
     <>
       <Stack.Screen options={{ title: "Your conversations" }} />
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <ScrollView
-          style={{ backgroundColor: theme.colors.background, flex: 1 }}
-        >
-          {allChats && allChats.length > 0 ? (
-            allChats.map((chat) => {
-              const destinationUser =
-                chat.UserOriginId === currentUser?.Id
-                  ? chat.UserDestination
-                  : chat.UserOrigin;
 
-              const lastMessage =
-                chat.ChatMessages && chat.ChatMessages.length > 0
-                  ? chat.ChatMessages[chat.ChatMessages.length - 1]
-                  : null;
+      <ThemedView style={{ height: "100%" }}>
+        <GestureHandlerRootView style={{ flex: 1, paddingTop: 50 }}>
+          <ScrollView
+            style={{ backgroundColor: theme.colors.background, flex: 1 }}
+          >
+            {allChats && allChats.length > 0 ? (
+              allChats.map((chat) => {
+                const destinationUser =
+                  chat.UserOriginId === currentUser?.Id
+                    ? chat.UserDestination
+                    : chat.UserOrigin;
 
-              const lastMessageText = lastMessage
-                ? lastMessage.Message.length > 30
-                  ? lastMessage.Message.slice(0, 30) + "..."
-                  : lastMessage.Message
-                : "No hay mensajes";
+                const lastMessage =
+                  chat.ChatMessages && chat.ChatMessages.length > 0
+                    ? chat.ChatMessages[chat.ChatMessages.length - 1]
+                    : null;
 
-              const lastMessageTime = lastMessage
-                ? new Date(lastMessage.MessageDateTime).toLocaleTimeString([], {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    hour12: false,
-                  })
-                : "";
+                const lastMessageText = lastMessage
+                  ? lastMessage.Message.length > 30
+                    ? lastMessage.Message.slice(0, 30) + "..."
+                    : lastMessage.Message
+                  : "No hay mensajes";
 
-              const notViewedMessagesCounter =
-                chat.ChatMessages?.filter(
-                  (message) =>
-                    !message.IsViewed && message.UserId !== currentUser.Id
-                ).length || 0;
+                const lastMessageTime = lastMessage
+                  ? new Date(lastMessage.MessageDateTime).toLocaleTimeString(
+                      [],
+                      {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        hour12: false,
+                      }
+                    )
+                  : "";
 
-              return (
-                <TouchableRipple
-                  key={chat.Id}
-                  onPress={() => {
-                    chatService.setActualConversation(chat);
-                    router.push("/Conversation");
+                const notViewedMessagesCounter =
+                  chat.ChatMessages?.filter(
+                    (message) =>
+                      !message.IsViewed && message.UserId !== currentUser.Id
+                  ).length || 0;
+
+                return (
+                  <TouchableRipple
+                    key={chat.Id}
+                    onPress={() => {
+                      chatService.setActualConversation(chat);
+                      router.push("/Conversation");
+                    }}
+                    style={[
+                      styles.chatItem,
+                      {
+                        borderBottomColor: theme.colors.surface,
+                        borderBottomWidth: 1,
+                      },
+                    ]}
+                  >
+                    <List.Item
+                      title={destinationUser?.Name || "Usuario"}
+                      titleStyle={{
+                        color: theme.colors.onSurface,
+                        fontWeight: "bold",
+                        fontSize: 18,
+                      }}
+                      description={lastMessageText}
+                      descriptionStyle={{
+                        marginTop: 4,
+                      }}
+                      left={() => (
+                        <Avatar.Image
+                          size={50}
+                          source={{
+                            uri: `${SERVER_IMAGE_URL}/${
+                              destinationUser?.AvatarImageUrl || "default.png"
+                            }`,
+                          }}
+                          style={{
+                            backgroundColor: "transparent",
+                            borderColor: theme.colors.outline,
+                            borderWidth: 1,
+                            marginLeft: 12,
+                          }}
+                        />
+                      )}
+                      right={() => (
+                        <View>
+                          <Text>{lastMessageTime}</Text>
+                          {notViewedMessagesCounter > 0 && (
+                            <View
+                              style={[
+                                { backgroundColor: theme.colors.primary },
+                                styles.counter,
+                              ]}
+                            >
+                              <Text>{notViewedMessagesCounter}</Text>
+                            </View>
+                          )}
+                        </View>
+                      )}
+                      style={{ paddingVertical: 8 }}
+                    />
+                  </TouchableRipple>
+                );
+              })
+            ) : (
+              <View style={styles.emptyContainer}>
+                <Text
+                  style={{
+                    color: theme.colors.onSurfaceVariant,
+                    ...theme.fonts.headlineSmall,
+                    marginBottom: 8,
                   }}
-                  style={[
-                    styles.chatItem,
-                    {
-                      borderBottomColor: theme.colors.surface,
-                      borderBottomWidth: 1,
-                    },
-                  ]}
                 >
-                  <List.Item
-                    title={destinationUser?.Name || "Usuario"}
-                    titleStyle={{
-                      color: theme.colors.onSurface,
-                      fontWeight: "bold",
-                      fontSize: 18,
-                    }}
-                    description={lastMessageText}
-                    descriptionStyle={{
-                      marginTop: 4,
-                    }}
-                    left={() => (
-                      <Avatar.Image
-                        size={50}
-                        source={{
-                          uri: `${SERVER_IMAGE_URL}/${
-                            destinationUser?.AvatarImageUrl || "default.png"
-                          }`,
-                        }}
-                        style={{
-                          backgroundColor: "transparent",
-                          borderColor: theme.colors.outline,
-                          borderWidth: 1,
-                          marginLeft: 12,
-                        }}
-                      />
-                    )}
-                    right={() => (
-                      <View>
-                        <Text>{lastMessageTime}</Text>
-                        {notViewedMessagesCounter > 0 && (
-                          <View
-                            style={[
-                              { backgroundColor: theme.colors.primary },
-                              styles.counter,
-                            ]}
-                          >
-                            <Text>{notViewedMessagesCounter}</Text>
-                          </View>
-                        )}
-                      </View>
-                    )}
-                    style={{ paddingVertical: 8 }}
-                  />
-                </TouchableRipple>
-              );
-            })
-          ) : (
-            <View style={styles.emptyContainer}>
-              <Text
-                style={{
-                  color: theme.colors.onSurfaceVariant,
-                  ...theme.fonts.headlineSmall,
-                  marginBottom: 8,
-                }}
-              >
-                No tienes chats
-              </Text>
+                  No tienes chats
+                </Text>
 
-              <Text
-                style={{
-                  color: theme.colors.onSurfaceVariant,
-                  ...theme.fonts.bodyMedium,
-                  opacity: 0.7,
-                  textAlign: "center",
-                  paddingHorizontal: 20,
-                }}
-              >
-                Envía un mensaje a un entrenador para iniciar una conversación.
-              </Text>
-            </View>
-          )}
-        </ScrollView>
-      </GestureHandlerRootView>
+                <Text
+                  style={{
+                    color: theme.colors.onSurfaceVariant,
+                    ...theme.fonts.bodyMedium,
+                    opacity: 0.7,
+                    textAlign: "center",
+                    paddingHorizontal: 20,
+                  }}
+                >
+                  Envía un mensaje a un entrenador para iniciar una
+                  conversación.
+                </Text>
+              </View>
+            )}
+          </ScrollView>
+        </GestureHandlerRootView>
+      </ThemedView>
     </>
   );
 }
